4955b41e5b4a259ce1cfa8d70fe4babb9eeb0f1b
debbie <debbalbo@Hotmail.com>
2015-11-10 10:25:10 -0800

    initial files

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<!doctype html>
+<html>
+<head>
+	<title>WorldWideWeather</title>
+!
+	<meta charset="utf-8" />
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	!
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/
+	bootstrap.min.css">
+	!
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/
+	bootstrap-theme.min.css">
+</head>
+
+<body>
+
+
+
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>	
